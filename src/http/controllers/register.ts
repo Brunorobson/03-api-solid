80f3ff57ed6881b54ae1fe762689eea76aef14b0
Brunorobson <bruno.assis@alu.unibalsas.edu.br>
2026-03-17 17:09:17 -0300

    Hash da senha e validação

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
+import { hash } from 'bcryptjs'
 import { z } from 'zod'
 import { prisma } from '../../lib/prisma.js'
 
@@ -11,11 +12,23 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
+  const password_hash = await hash(password, 6)
+
+  const userWithSamEmail = await prisma.user.findUnique({
+    where: {
+        email,
+    },
+  })
+
+  if (userWithSamEmail){
+    return reply.status(409).send("Email duplicado")
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     },
   })
 
