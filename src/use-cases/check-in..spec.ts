dbbfbbf2bfe2f7e14f38e02948394c6c182fd366
Brunorobson <bruno.assis@alu.unibalsas.edu.br>
2026-03-23 11:56:53 -0300

    TDD & Mocking

--- src/use-cases/check-in..spec.ts
+++ src/use-cases/check-in..spec.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it, beforeEach } from 'vitest'
+import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository.js'
 import { CheckInUseCase } from './checkin.js'
 
@@ -8,18 +8,35 @@ describe('Check-in use case', () => {
     beforeEach(() => {
         checkinRepository = new InMemoryCheckInsRepository
         stu = new CheckInUseCase(checkinRepository)
+
+        vi.useFakeTimers()
     }
 
 )
+
+afterEach(() => {
+    vi.useRealTimers
+})
     it('should be able to check in', async() =>{
-
-
         const { checkIn } = await stu.execute({
             gymId: 'gym-01',
             userId: 'user-01',
         })
 
-
         expect(checkIn.id).toEqual(expect.any(String))
+    })
+
+    it('should not be able to check in twice in the same day', async() =>{
+
+        vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+        const { checkIn } = await stu.execute({
+            gymId: 'gym-01',
+            userId: 'user-01',
+        })
+
+        await expect(() => stu.execute({
+            gymId: 'gym-01',
+            userId: 'user-01',
+        })).rejects.toBeInstanceOf(Error)
     })
 })
--- src/use-cases/checkin.ts
+++ src/use-cases/checkin.ts
@@ -19,6 +19,16 @@ async execute({
     userId,
     gymId
 }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse>{
+
+    const checkInOnSameDay = await this.CheckInsRepository.findByUserIdOnDate(
+        userId,
+        new Date(),
+    )
+
+    if (checkInOnSameDay){
+        throw new Error()
+    }
+    
     const checkIn = await this.CheckInsRepository.create({
         gym_id: gymId,
         user_id: userId
